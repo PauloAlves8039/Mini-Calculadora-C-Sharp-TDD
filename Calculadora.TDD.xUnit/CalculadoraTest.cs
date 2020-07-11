d3b148a602174d71f9b503917d5f3e525858fa59
PauloAlves8039 <pj38alves@gmail.com>
2020-07-11 12:34:40 -0300

    Implementando métodos de testes

--- Calculadora.TDD.xUnit/CalculadoraTest.cs
+++ Calculadora.TDD.xUnit/CalculadoraTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace Calculadora.TDD.xUnit
 {
@@ -12,6 +7,45 @@ namespace Calculadora.TDD.xUnit
     /// </summary>
     public class CalculadoraTest
     {
+        /// <summary>
+        /// Instância da classe Calculadora.
+        /// </summary>
+        Calculadora _calc = new Calculadora();
 
+        /// <summary>
+        /// Testa o resultado da soma entre dois valores inteiros.
+        /// </summary>
+        [Fact]
+        public void SomarTest()
+        {
+            Assert.Equal(8, _calc.Somar(4, 4));
+        }
+
+        /// <summary>
+        /// Testa o resultado da subtração entre dois valores inteiros.
+        /// </summary>
+        [Fact]
+        public void SubtrairTest()
+        {
+            Assert.Equal(3, _calc.Subtrair(6, 3));
+        }
+
+        /// <summary>
+        /// Testa o resultado da multiplicação entre dois valores inteiros.
+        /// </summary>
+        [Fact]
+        public void MultiplicarTest()
+        {
+            Assert.Equal(10, _calc.Multiplicar(5, 2));
+        }
+
+        /// <summary>
+        /// Testa o resultado da divisão entre dois valores inteiros.
+        /// </summary>
+        [Fact]
+        public void DividirTest()
+        {
+            Assert.Equal(2, _calc.Dividir(4, 2));
+        }
     }
 }
