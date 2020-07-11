ff66d2586fd6d4b7eb7e45fa32b16dbd9cc7464f
PauloAlves8039 <pj38alves@gmail.com>
2020-07-11 12:13:50 -0300

    Adicionando aplicação de teste

---
+++ Calculadora.TDD.xUnit/CalculadoraTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Calculadora.TDD.xUnit
+{
+    /// <summary>
+    /// Responsável por realizar testes de unidade referentes a classe Calculadora.
+    /// </summary>
+    public class CalculadoraTest
+    {
+
+    }
+}
