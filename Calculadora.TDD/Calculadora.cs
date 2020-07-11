57d31b9e4aaeff546d8477e104baee2bf5bc93fc
PauloAlves8039 <pj38alves@gmail.com>
2020-07-11 11:47:27 -0300

    Adicionando aplicação no repositório

---
+++ Calculadora.TDD/Calculadora.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Calculadora.TDD
+{
+    /// <summary>
+    /// Classe responsável por representar a entidade calculadora.
+    /// </summary>
+    public class Calculadora
+    {
+        /// <summary>
+        /// Representa a proriedade com valor 1.
+        /// </summary>
+        public int Valor1 { get; set; }
+
+        /// <summary>
+        /// Representa a proriedade com valor 2.
+        /// </summary>
+        public int Valor2 { get; set; }
+
+        /// <summary>
+        /// Contrutor responsável pela inicialização das proriedades.
+        /// </summary>
+        /// <param name="valor1">Recebe a prorriedade com valor 1.</param>
+        /// <param name="valor2">Recebe a prorriedade com valor 1.</param>
+        public Calculadora(int valor1, int valor2)
+        {
+            Valor1 = valor1;
+            Valor2 = valor2;
+        }
+
+        /// <summary>
+        /// Construtor vázio adicionado para uso em classe de teste.
+        /// </summary>
+        public Calculadora()
+        {
+
+        }
+
+        /// <summary>
+        /// Responsável por somar dois valores.
+        /// </summary>
+        /// <param name="valor1">Indica o recebimento do primeiro valor.</param>
+        /// <param name="valor2">Indica o recebimento do segundo valor.</param>
+        /// <returns>A soma do valor 1 com o valor 2.</returns>
+        public int Somar(int valor1, int valor2)
+        {
+            try
+            {
+                return valor1 + valor2;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Responsável por subtrair dois valores.
+        /// </summary>
+        /// <param name="valor1">Indica o recebimento do primeiro valor.</param>
+        /// <param name="valor2">Indica o recebimento do segundo valor.</param>
+        /// <returns>A subtração do valor 1 com o valor 2</returns>
+        public int Subtrair(int valor1, int valor2)
+        {
+            try
+            {
+                return valor1 - valor2;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Responsável por multiplicar dois valores.
+        /// </summary>
+        /// <param name="valor1">Indica o recebimento do primeiro valor.</param>
+        /// <param name="valor2">Indica o recebimento do segundo valor.</param>
+        /// <returns>A multiplicação do valor 1 com o valor 2.</returns>
+        public int Multiplicar(int valor1, int valor2)
+        {
+            try
+            {
+                return valor1 * valor2;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Responsável por dividir dois valores.
+        /// </summary>
+        /// <param name="valor1">Indica o recebimento do primeiro valor.</param>
+        /// <param name="valor2">Indica o recebimento do segundo valor.</param>
+        /// <returns>A divisão do valor 1 com o valor 2.</returns>
+        public int Dividir(int valor1, int valor2)
+        {
+            try
+            {
+                return valor1 / valor2;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Calculadora.TDD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculadora.TDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********** MINI CALCULADORA **********");
+
+            Console.Write("Digite o primeiro valor: ");
+            int v1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            int v2 = int.Parse(Console.ReadLine());
+
+            Calculadora c = new Calculadora(v1, v2);
+
+            Console.WriteLine("\n********** RESULTADO **********");
+
+            Console.WriteLine("A soma é: " + c.Somar(v1, v2) 
+                              + "\nA subtração é: " + c.Subtrair(v1, v2) 
+                              + "\nA multiplicação é: " + c.Multiplicar(v1, v2) 
+                              + "\nA divisão é: " + c.Dividir(v1, v2));
+
+            Console.ReadKey();
+        }
+    }
+}
